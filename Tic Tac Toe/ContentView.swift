d31eaba80340ab683dbd9ad961323df7eec902f6
Kadir Ince <kadirincedev@gmail.com>
2020-10-02 17:34:42 +0300

    Created rotation effect for clicked card

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -31,15 +31,26 @@ struct Home: View {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
                 ForEach(0 ..< 9, id: \.self) { index in
                     ZStack {
+                        Color.blue
+
                         Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
 
                         Text(moves[index])
                             .font(.system(size: 55))
                             .fontWeight(.heavy)
-                            .foregroundColor(.black)
+//                            .foregroundColor(.black)
+                            .foregroundColor(.white)
                     }
                     .frame(width: (screenWidth - 60)/3, height: (screenWidth - 60)/3)
                     .cornerRadius(15)
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] == "" ? 180 : 0),
+                        axis: (x: 0.0, y: 1.0, z: 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 1.0
+                    )
                     .onTapGesture(perform: {
                         withAnimation(.easeIn(duration: 0.5)) {
                             if moves[index] == "" {
