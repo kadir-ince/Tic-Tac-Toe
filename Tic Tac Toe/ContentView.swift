8aa3bcc5b5a9e858739fcc321243901f2ba51baf
Kadir Ince <kadirincedev@gmail.com>
2020-10-02 17:56:01 +0300

    Done App's Functionality

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -26,6 +26,9 @@ struct ContentView_Previews: PreviewProvider {
 struct Home: View {
     @State var moves: [String] = Array(repeating: "", count: 9)
     @State var isPlaying = true
+    @State var gameOver = false
+    @State var showMessage = ""
+
     var body: some View {
         VStack {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
@@ -63,6 +66,56 @@ struct Home: View {
             }
             .padding()
         }
+        .onChange(of: moves, perform: { _ in
+            checkWinner()
+        })
+        .alert(isPresented: $gameOver, content: {
+            Alert(title: Text("Winner"), message: Text(showMessage), dismissButton: .destructive(Text("Play Again"), action: {
+                withAnimation(.easeIn(duration: 0.5)) {
+                    moves.removeAll()
+                    moves = Array(repeating: "", count: 9)
+                    isPlaying = true
+                }
+            }))
+        })
+    }
+
+    func checkWinner() {
+        if checkMoves(player: "X") {
+            showMessage = "Player X Won 🎉"
+            gameOver.toggle()
+        }
+        if checkMoves(player: "O") {
+            showMessage = "Player O Won 🎉"
+            gameOver.toggle()
+        }
+    }
+
+    func checkMoves(player: String) -> Bool {
+        // horizontal check
+        for i in stride(from: 0, to: 9, by: 3) {
+            if moves[i] == player && moves[i+1] == player && moves[i+2] == player {
+                return true
+            }
+        }
+
+        // vertical check
+        for i in 0 ... 2 {
+            if moves[i] == player && moves[i+3] == player && moves[i+6] == player {
+                return true
+            }
+        }
+
+        // diagonal check
+
+        if moves[0] == player && moves[4] == player && moves[8] == player {
+            return true
+        }
+        if moves[2] == player && moves[4] == player && moves[6] == player {
+            return true
+        }
+
+        return false
     }
 }
 
